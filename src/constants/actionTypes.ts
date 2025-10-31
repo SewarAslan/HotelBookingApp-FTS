f2b4b3ede8dcb922578bdb1f97e9575b26c3cdca
SewarAslan <sewaraslan02@gmail.com>
2025-10-31 16:21:09 +0200

    implement authReducer with immer

---
+++ src/constants/actionTypes.ts
@@ -0,0 +1,4 @@
+export const AUTH_ACTIONS = {
+  LOGIN_SUCCESS: "LOGIN_SUCCESS",
+  LOGOUT: "LOGOUT",
+} as const;
---
+++ src/features/auth/context/authReducer.ts
@@ -0,0 +1,24 @@
+import { Draft } from "Immer";
+import type { AuthState, AuthAction } from "../types/AuthReducerType";
+import { AUTH_ACTIONS } from "../../../constants/actionTypes";
+
+export const initialAuthState: AuthState = {
+  token: null,
+  userType: null,
+  isAuthenticated: false,
+};
+
+function authReducer(draft: Draft<AuthState>, action: AuthAction) {
+  switch (action.type) {
+    case AUTH_ACTIONS.LOGIN_SUCCESS:
+      draft.token = action.payload.token;
+      draft.userType = action.payload.userType;
+      draft.isAuthenticated = true;
+      break;
+
+    case AUTH_ACTIONS.LOGOUT:
+      (draft.token = null),
+        (draft.userType = null),
+        draft.isAuthenticated - false;
+  }
+}
