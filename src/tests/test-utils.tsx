5a1be7a90c57979b603164436c90be8ed50d83fd
SewarAslan <sewaraslan02@gmail.com>
2025-12-08 14:04:40 +0200

    setup testing

--- src/tests/test-utils.tsx
+++ src/tests/test-utils.tsx
@@ -1,7 +1,32 @@
-import { ThemeProvider } from "@mui/material/styles";
-import { render } from "@testing-library/react";
 import { lightTheme } from "../styles/theme";
+import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { render } from "@testing-library/react";
+import { store as defaultStore } from "../store/store";
+import type { PropsWithChildren } from "react";
+import type { Store } from "@reduxjs/toolkit";
 
-export function renderWithTheme(ui: React.ReactNode) {
-  return render(<ThemeProvider theme={lightTheme}>{ui}</ThemeProvider>);
+interface RenderOptions {
+  storeOverride?: Store;
+  [key: string]: unknown;
+}
+
+export function renderWithProviders(
+  ui: React.ReactElement,
+  { storeOverride, ...options }: RenderOptions = {}
+) {
+  const usedStore = storeOverride || defaultStore;
+
+  function Wrapper({ children }: PropsWithChildren) {
+    return (
+      <Provider store={usedStore}>
+        <BrowserRouter>
+          <ThemeProvider theme={lightTheme}>{children}</ThemeProvider>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+
+  return render(ui, { wrapper: Wrapper, ...options });
 }
